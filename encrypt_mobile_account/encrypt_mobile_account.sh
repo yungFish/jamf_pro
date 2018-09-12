0ebafa1345cce87f13c4ff27aa42fd2daad216d4
yungFish <37179401+yungFish@users.noreply.github.com>
2018-09-12 03:13:39 -0500

    Added encrypt_mobile_account.sh

---
+++ encrypt_mobile_account/encrypt_mobile_account.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mobileAccountList=$( dscl . list /Users OriginalNodeName | awk '{print $1}' 2>/dev/null )
+currentUser=$( /bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }' )
+
+accountStatus="local"
+
+for mobileAccount in ${mobileAccountList}; do
+	
+	[[ ${mobileAccount} == ${currentUser} ]] && accountStatus="mobile"
+	
+done
+
+if [[ ${accountStatus} == "mobile" ]]; then
+
+# 	jamf policy -event encrypt
+	
+fi
