4c752207e31217a6a919c939ecaf24997d33efb0
yungFish <37179401+yungFish@users.noreply.github.com>
2019-04-09 09:56:13 -0500

    Update SelfService_Ticketing.sh

--- SelfService_Help_Request/SelfService_Ticketing.sh
+++ SelfService_Help_Request/SelfService_Ticketing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 thisSerial=$( ioreg -l | grep IOPlatformSerialNumber | awk -F '"' '{print $4}' )
-currentDate=$( date "+%m/%d/%y %H:%M:%S" )
+currentDate=$( date "+%y-%m-%d_%H:%M:%S" )
 
 ticketDescription=$( osascript << EOF
 	text returned of (display dialog "Please describe your issue/request." default answer "I just wanted to say how much I appreciate y'all in my IT department." buttons {"OK"} default button 1)
@@ -13,7 +13,7 @@ EOF
 # value in the XML variable for the EA name (in this case "SelfService_Help_Request") is
 # updated to match your attribute.
 
-XMLTOWRITE="<computer><extension_attributes><extension_attribute><name>SelfService_Help_Request</name><value>( ${currentDate} ) - ${ticketDescription}</value></extension_attribute></extension_attributes></computer>"
+XMLTOWRITE="<computer><extension_attributes><extension_attribute><name>SelfService_Help_Request</name><value>[${currentDate}] - ${ticketDescription}</value></extension_attribute></extension_attributes></computer>"
 
 curl -sku apiuser:password \
 	https://company.jamfcloud.com/JSSResource/computers/serialnumber/${thisSerial}/subset/extensionattributes \
