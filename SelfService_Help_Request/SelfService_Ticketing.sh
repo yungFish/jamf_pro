59e807dddf291df9ea6cd2301fc76b064f2311a7
yungFish <37179401+yungFish@users.noreply.github.com>
2019-04-10 02:17:46 -0500

    grammar corrections

--- SelfService_Help_Request/SelfService_Ticketing.sh
+++ SelfService_Help_Request/SelfService_Ticketing.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ##########################################################################################
 #
-# This script uses the API to update an Extension Attribute via the EA name included in
-# the $XMLTOWRITE variable below. Ensure the value in that XML variable for the EA name
+# This script uses the API to update an Extension Attribute by the EA name included in
+# the $XMLTOWRITE variable below. Ensure the value in the $EAName variable
 # (in this case "SelfService_Help_Request") is updated for your attribute/environment.
 #
 ##########################################################################################
 #
 # 2019-04-09:	Cleaned up user cancel/escape input
-#				Added the option to set defaultTicketText to parameter 4
+#		Added the option to set defaultTicketText to parameter 4
 #
 ##########################################################################################
 
@@ -45,8 +45,6 @@ if [[ -n "${ticketDescription}" ]]; then
 	
 	XMLTOWRITE="<computer><extension_attributes><extension_attribute><name>${EAName}</name><value>[$( date '+%Y-%m-%d_%H:%M:%S' )] - ${ticketDescription}</value></extension_attribute></extension_attributes></computer>"
 	curl -sku ${JSSAdmin}:${JSSPassw} ${JSSURL}/JSSResource/computers/serialnumber/${thisSerial}/subset/extensionattributes -H "Content-type: text/xml" -X PUT -d "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>${XMLTOWRITE}"
-	
-	exit 0
 fi
 
 exit 0
