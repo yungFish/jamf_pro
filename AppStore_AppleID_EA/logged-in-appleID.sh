1b99852db9d278228fab358389b36c4cd76c14c7
yungFish <37179401+yungFish@users.noreply.github.com>
2019-04-28 22:34:08 -0400

    Update logged-in-appleID.sh

--- AppStore_AppleID_EA/logged-in-appleID.sh
+++ AppStore_AppleID_EA/logged-in-appleID.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# Determine the logged in user
 CURRENTUSER=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
 
+# Find the Apple ID(s) used in the App Store by the end user
 appleIDList=$( defaults read /Users/${CURRENTUSER}/Library/Preferences/com.apple.commerce.plist | grep appleId | awk -F '["]' '{print $2}' )
 
-if [[ -z ${appleIDList} ]]; then
-	EAResult="No AppleID found"
-fi
+[[ -z ${appleIDList} ]] && EAResult="No AppleID found"
 
 while read -r; do
 	appleIDarray+=("$REPLY")
