ac806538dbde1f02c27c39eeacdc1393aa8d31b1
yungFish <37179401+yungFish@users.noreply.github.com>
2019-05-15 04:15:48 -0400

    added Populate-scoping-EA

---
+++ Populate-scoping-EA/Populate-scoping-EA.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+thisSerial=$( ioreg -l | grep IOPlatformSerialNumber | awk -F '"' '{print $4}' )
+
+JSSURL="https://yungfish.jamfcloud.com"
+JSSADMIN='apiadmin'
+JSSPassw='partypartyjoinusjoinus'
+EAName="Restrictions-Temp-Removal"
+
+XMLTOWRITE="<computer><extension_attributes><extension_attribute><name>${EAName}</name><value>[Admin login - $( date '+%Y-%m-%d_%H:%M:%S' ) ]</value></extension_attribute></extension_attributes></computer>"
+	curl -sku ${JSSAdmin}:${JSSPassw} ${JSSURL}/JSSResource/computers/serialnumber/${thisSerial}/subset/extensionattributes -H "Content-type: text/xml" -X PUT -d "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>${XMLTOWRITE}"
+	
